7ef9e9b73847d6b7fd1190c5fc1530dcfdf22018
Asogwa Emmanuel <asogwaemmanuel36@gmail.com>
2025-01-05 21:48:49 +0100

    revise constants to match revised types

--- src/common/llm-constants.c
+++ src/common/llm-constants.c
@@ -14,6 +14,7 @@ const LLMClientConfig DEFAULT_LLMCLIENT_CONFIG = {
         .max_tokens = MAX_LLM_OUTPUT_TOKENS,
         .temperature = 0.7f,
         .json_response_schema = NULL,
+        .api_endpoint = NULL,
         .top_p = 1,
         .top_k = 50
     },
@@ -25,6 +26,9 @@ const LLMClientConfig DEFAULT_LLMCLIENT_CONFIG = {
             .nbytes = 0,
             .uri = NULL,
             },       
+        .response = {
+            .return_raw = 0,
+        },    
     },
     .user_state = NULL                      // optional user state pointer
 };
@@ -40,3 +44,34 @@ const ProviderFeaturePool provider_google_gemini = {
     .num_supported_features = 4,
     .provider = GOOGLE_GEMINI,
 };
+
+
+const ProviderFeaturePool provider_openai_gpt = {
+    .supported_features = {
+        TEXT_INPUT,
+        TEXT_INPUT_WITH_REMOTE_IMG,
+        TEXT_INPUT_WITH_LOCAL_IMG,
+        TEXT_INPUT_WITH_LOCAL_AUDIO,
+        TEXT_INPUT_WITH_STRUCTURED_OUTPUT,
+    },
+    .num_supported_features = 4,
+    .provider = OPENAI_GPT,
+};
+
+const ProviderFeaturePool provider_qroq_ = {
+    .supported_features = {
+        TEXT_INPUT,
+    },
+    .num_supported_features = 1,
+    .provider = GROK,
+};
+
+//helper function to check if feature is supported
+int _is_feature_supported(GlobalFeaturePool gfeature, ProviderFeaturePool pfeature) {
+    for (size_t i = 0; i < pfeature.num_supported_features; i++) {
+        if (pfeature.supported_features[i] == gfeature) {
+            return 1;
+        }
+    }
+    return 0;
+}
